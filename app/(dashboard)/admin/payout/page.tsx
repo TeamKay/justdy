22b6547abdc786586eba575516ba514fe09d68c2
Justice Kangah <justicekangah@gmail.com>
2026-05-12 14:17:36 -0500

    sss

--- app/(dashboard)/admin/payout/page.tsx
+++ app/(dashboard)/admin/payout/page.tsx
@@ -1,38 +1,28 @@
-import { PendingPayouts } from "@/app/_components/PendingPayouts";
-import { getPendingPayouts } from "@/app/actions/admin";
+// import { PendingPayouts } from "@/app/_components/PendingPayouts";
+// import { getPendingPayouts } from "@/app/actions/admin";
 
 export default async function AdminPage() {
-  const [pendingPayoutsData] = await Promise.all([getPendingPayouts()]);
+  //const [pendingPayoutsData] = await Promise.all([getPendingPayouts()]);
 
-  const payouts =
-    pendingPayoutsData.payouts?.map((p) => ({
-      id: p.id,
-      educator: {
-        name: p.educator.name,
-        email: p.educator.email,
-        specialty: p.educator.specialty ?? "General",
-        credits: p.educator.credits,
-      },
-      credits: p.credits,
-      amount: p.netAmount + p.platformFee, // or define properly if backend has gross
-      platformFee: p.platformFee,
-      netAmount: p.netAmount,
-      paypalEmail: p.paypalEmail,
-      createdAt: p.createdAt,
-    })) || [];
+  // const payouts =
+  //   pendingPayoutsData.payouts?.map((p) => ({
+  //     id: p.id,
+  //     educator: {
+  //       name: p.educator.name,
+  //       email: p.educator.email,
+  //       specialty: p.educator.specialty ?? "General",
+  //       credits: p.educator.credits,
+  //     },
+  //     credits: p.credits,
+  //     amount: p.netAmount + p.platformFee, // or define properly if backend has gross
+  //     platformFee: p.platformFee,
+  //     netAmount: p.netAmount,
+  //     paypalEmail: p.paypalEmail,
+  //     createdAt: p.createdAt,
+  //   })) || [];
 
-  return <PendingPayouts payouts={payouts} />;
+  return (
+    // <PendingPayouts payouts={payouts}
+    <div>kkkkkkk</div>
+  );
 }
-
-// import { getPendingPayouts } from "@/app/actions/admin";
-// import { PendingPayouts } from "@/app/_components/PendingPayouts";
-
-// export default async function AdminPage() {
-//   const [pendingPayoutsData] = await Promise.all([getPendingPayouts()]);
-
-//   return (
-//     <>
-//       <PendingPayouts payouts={pendingPayoutsData.payouts || []} />
-//     </>
-//   );
-// }
