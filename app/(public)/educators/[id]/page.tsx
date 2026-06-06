64f0caf307e6c1ad65512fa07726ed8ffe92b7a3
Justice Kangah <justicekangah@gmail.com>
2026-06-05 21:15:37 -0500

    hhh

--- app/(public)/educators/[id]/page.tsx
+++ app/(public)/educators/[id]/page.tsx
@@ -1,37 +1,37 @@
-import EducatorProfile from "@/app/_components/EducatorProfile";
-import {
-  getAvailableTimeSlots,
-  getEducatorById,
-} from "@/app/actions/appointments";
-import { redirect } from "next/navigation";
+// import {
+//   getAvailableTimeSlots,
+//   getEducatorById,
+// } from "@/app/actions/appointments";
+// import { redirect } from "next/navigation";
 
-interface PageProps {
-  params: { id: string };
-}
+// interface PageProps {
+//   params: { id: string };
+// }
 
-export default async function EducatorIDPage({ params }: PageProps) {
-  const { id } = await params;
+export default async function EducatorIDPage() {
+  // const { id } = await params;
 
-  let educatorData;
-  let slotsData;
+  // let educatorData;
+  // let slotsData;
 
-  try {
-    [educatorData, slotsData] = await Promise.all([
-      getEducatorById(id),
-      getAvailableTimeSlots(id),
-    ]);
-  } catch (error) {
-    console.error("Error loading educator profile", error);
-    redirect("/educators");
-  }
+  // try {
+  //   [educatorData, slotsData] = await Promise.all([
+  //     getEducatorById(id),
+  //     getAvailableTimeSlots(id),
+  //   ]);
+  // } catch (error) {
+  //   console.error("Error loading educator profile", error);
+  //   redirect("/educators");
+  // }
 
   return (
-    <EducatorProfile
-      educator={{
-        ...educatorData.educator,
-        imageUrl: educatorData.educator.imageUrl ?? undefined,
-      }}
-      availableDays={slotsData.days || []}
-    />
+    <div>mmmmmmmmm</div>
+    // <EducatorProfile
+    //   educator={{
+    //     ...educatorData.educator,
+    //     imageUrl: educatorData.educator.imageUrl ?? undefined,
+    //   }}
+    //   availableDays={slotsData.days || []}
+    // />
   );
 }
